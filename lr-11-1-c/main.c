09106a33155560f34849a19a9bf88b14e67943d9
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-05-15 15:03:13 +0300

    made it using pointers

--- lr-11-1-c/main.c
+++ lr-11-1-c/main.c
@@ -11,7 +11,7 @@ int main() {
 
     do {
         printf("Введіть кількість стовпців: ");
-        if (scanf_s("%d", &N) != 1) {
+        if (scanf_s("%d", &N) != 1 || N <= 0) {
             printf("Некоректне введення! Спробуйте ще раз.\n");
             while (getchar() != '\n');
         }
@@ -19,7 +19,7 @@ int main() {
 
     do {
         printf("Введіть кількість рядків: ");
-        if (scanf_s("%d", &M) != 1) {
+        if (scanf_s("%d", &M) != 1 || M <= 0) {
             printf("Некоректне введення! Спробуйте ще раз.\n");
             while (getchar() != '\n');
         }
@@ -32,11 +32,11 @@ int main() {
     }
 
     for (int i = 0; i < M; i++) {
-        A[i] = (int*)malloc(N * sizeof(int));
-        if (A[i] == NULL) {
+        *(A + i) = (int*)malloc(N * sizeof(int));
+        if (*(A + i) == NULL) {
             printf("Помилка виділення пам'яті!\n");
             for (int j = 0; j < i; j++) {
-                free(A[j]);
+                free(*(A + j));
             }
             free(A);
             return 1;
@@ -47,7 +47,7 @@ int main() {
     if (B == NULL) {
         printf("Помилка виділення пам'яті!\n");
         for (int i = 0; i < M; i++) {
-            free(A[i]);
+            free(*(A + i));
         }
         free(A);
         return 1;
@@ -57,7 +57,7 @@ int main() {
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
             printf("A[%d][%d] = ", i, j);
-            if (scanf_s("%d", &A[i][j]) != 1) {
+            if (scanf_s("%d", *(A + i) + j) != 1) {
                 printf("Некоректне введення! Спробуйте ще раз.\n");
                 while (getchar() != '\n');
                 j--;
@@ -68,21 +68,21 @@ int main() {
     for (int i = 0; i < M; i++) {
         int min = INT_MAX;
         for (int j = 0; j < N; j++) {
-            if (A[i][j] < min) {
-                min = A[i][j];
+            if (*(*(A + i) + j) < min) {
+                min = *(*(A + i) + j);
             }
         }
-        B[i] = min;
+        *(B + i) = min;
     }
 
     printf("Мінімальні значення в рядках: ");
     for (int i = 0; i < M; i++) {
-        printf("%d ", B[i]);
+        printf("%d ", *(B + i));
     }
     printf("\n");
 
     for (int i = 0; i < M; i++) {
-        free(A[i]);
+        free(*(A + i));
     }
     free(A);
     free(B);
